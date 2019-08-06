8c6f0f8b54ca367b3e1e8395c8b2214ef63bb16d
Alejandrosp23 <alejandrosantos1123@gmail.com>
2019-08-07 02:36:30 +0400

    +Slider

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -111,14 +111,24 @@ NOTICIA;
 </body>
 
 <style>
+.carousel-item {
+  height: 65vh;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+/*
   .carousel-inner img {
       width: 640px;
       max-height: 400px;
 	  margin: auto;
-  }
+  }*/
 
   .carousel {
-	  margin-top: 105px;
+	  margin-top: 103px;
   }
 </style>
 <script>
